b994c57e90c1814afc718feb3f8909dbded23593
tanyshk <tanyshk@gmail.com>
2023-09-22 16:08:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // This is the "Offline copy of pages" service worker
-
+import './registerServiceWorker';
 const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
